0164fa3203fc0c105aa1e8103b24b8cf41a59c5a
Elcin Quliyev <wdplay75@gmail.com>
2022-10-29 17:18:58 +0400

    add

---
+++ main.js
@@ -0,0 +1,13 @@
+let inUnersty = 90;
+let enterscore = 100;
+let (totalscore)=inUnersty + enterscore
+
+if (inuniUnersty === 90) {
+  console.log("A");
+} else if (inuniUnersty >= 75) {
+  console.log("B");
+} else if (inuniUnersty >= 60) {
+  console.log("C");
+} else {
+  console.log("Kesildiniz");
+}
